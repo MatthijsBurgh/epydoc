27b5afe0a9c6eb0e933e0c7ba68f7dabddaf31dc
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-12-24 22:51:28 +0100

    maxint -> maxsize

--- src/epydoc/docstringparser.py
+++ src/epydoc/docstringparser.py
@@ -954,7 +954,7 @@ def unindent_docstring(docstring):
     lines = docstring.expandtabs().split('\n')
 
     # Find minimum indentation of any non-blank lines after first line.
-    margin = sys.maxint
+    margin = sys.maxsize
     for line in lines[1:]:
         content = len(line.lstrip())
         if content:
@@ -963,7 +963,7 @@ def unindent_docstring(docstring):
     # Remove indentation.
     if lines:
         lines[0] = lines[0].lstrip()
-    if margin < sys.maxint:
+    if margin < sys.maxsize:
         for i in range(1, len(lines)): lines[i] = lines[i][margin:]
     # Remove any trailing (but not leading!) blank lines.
     while lines and not lines[-1]:

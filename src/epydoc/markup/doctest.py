4cbd51bce9c3b8e9a52d0b44b315430611391cea
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-12-27 11:13:11 +0100

    (doctest) Use generator instead of list

--- src/epydoc/markup/doctest.py
+++ src/epydoc/markup/doctest.py
@@ -196,11 +196,11 @@ class DoctestColorizer:
             # Example output:
             if want:
                 if self.EXCEPT_RE.match(want):
-                    output += self.NEWLINE.join([self.markup(line, 'except')
-                                                 for line in want.split('\n')])
+                    output.append(self.NEWLINE.join(self.markup(line, 'except')
+                                                    for line in want.split('\n')))
                 else:
-                    output += self.NEWLINE.join([self.markup(line, 'output')
-                                                 for line in want.split('\n')])
+                    output.append(self.NEWLINE.join(self.markup(line, 'output')
+                                                    for line in want.split('\n')))
             # Update charno
             charno = m.end()
         # Add any remaining post-example text.
